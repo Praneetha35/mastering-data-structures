e595ab5c6705b0315c534a96c6c69bfa359b884f
Praneetha Chandra Prakash <67695133+Praneetha35@users.noreply.github.com>
2024-09-22 11:05:24 -0400

    Update singly_linked_list.py

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -21,7 +21,7 @@ class LinkedList:
         newNode = Node(data) # newNode.data -> 10, newNode.next -> None
         newNode.next = self.head # newNode.next -> None
         self.head = newNode # newNode.head -> newNode's address
-        
+
     
     def insertAtTheEnd(self,data):
         newNode = Node(data)
@@ -32,6 +32,7 @@ class LinkedList:
             while node.next is not None: # Use node.next is None because we need that last node, use node is None for others
                 node = node.next
             node.next = newNode
+    
     
     def insertAfter(self,data,x):
         node = self.head
@@ -45,8 +46,22 @@ class LinkedList:
             newNode = Node(data)
             newNode.next = node.next
             node.next = newNode
+    
+    def insertBefore(self,data,x):
+        node = self.head
+        while node is not None:
+            if node.data == x:
+                break
+            prevNode = node
+            node = node.next
+        if node is None:
+            print("Node is not present in the LinkedList")
+        else:
+            newNode = Node(data)
+            prevNode.next = newNode
+            newNode.next = node
         
-        # Delete node
+    # Delete node
         
     def deleteAtTheBeginning(self):
         if self.head is None:
@@ -54,7 +69,32 @@ class LinkedList:
         else:
             temp = self.head
             self.head = self.head.next
-            del temp
+
+    
+    def deleteAtTheEnd(self):
+        if self.head is None:
+            print("Linked List is empty")
+        elif self.head.next is None:
+            self.head = None # Check if only one element is there then make the 
+        else:
+            node = self.head
+            while node.next.next is not None:
+                node = node.next
+            node.next = None
+            
+    def deleteByValue(self,data):
+        node = self.head
+        if node is None:
+            print("Node is not present in the LinkedList")
+        elif node.data == data:
+            self.head = None
+        else:
+            while node is not None:
+                if node.data == data:
+                    prevNode.next = node.next
+                prevNode = node
+                node = node.next
+
     
             
 
@@ -65,15 +105,18 @@ linkedList.insertAtTheBeginning(20)
 linkedList.insertAtTheBeginning(30)
 linkedList.insertAtTheEnd(40)
 linkedList.insertAfter(50,20)
+linkedList.insertBefore(100,40)
 print("LinkedList Before Deletion:")
 linkedList.printLinkedList()
-linkedList.deleteAtTheBeginning()
-print("linkedList After Deletion:")
+# linkedList.deleteAtTheBeginning()
+
+# print("linkedList After Deletion:")
+# linkedList.printLinkedList()
+
+# linkedList.deleteAtTheEnd()
+# print("linkedList After Deletion at the End:")
+# linkedList.printLinkedList()
+
+linkedList.deleteByValue(20)
+print("linkedList After Deleting Value 20:")
 linkedList.printLinkedList()
-
-
-
-    
-        
-        
-            
